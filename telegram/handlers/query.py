4d863d77b4db508165a6d828deab6c2d5a8403cb
Gribstovich <Gribstovich@proton.me>
2024-07-27 01:12:01 +0500

    Updated handle_text_message and handle_deck_selection functions

--- telegram/handlers/query.py
+++ telegram/handlers/query.py
@@ -5,49 +5,70 @@ from aiogram.filters.callback_data import CallbackQuery
 from anki import anki_api
 from telegram.config import config
 from telegram.keyboards.deck_keyboard import generate_deck_keyboard
+from wooordhunt import parser
+
+word = None
 
 
 async def handle_text_message(message: types.Message) -> None:
     """
-    Handle all other text messages.
+    Handles text messages from users.
 
-    This handler checks if the message contains only one English word.
-    If the message does not meet this criterion, it sends a response indicating
-    that the message is not an English word.
+    This function processes messages to check if they contain a single English word.
+    If the message does not contain a valid English word, it sends a response indicating
+    that the message does not contain an English word. If the word is valid, it retrieves
+    its details and sends a formatted reply with the word's information and an inline
+    keyboard for deck selection.
 
-    :param message: The message object containing the text message.
+    :param message: The message object containing the text message from the user.
     """
     text = message.text
     if not (text.isalpha() and text.isascii() and len(text.split()) == 1):
         await message.reply(
             config.NOT_ENGLISH_WORD_MESSAGE.format(message.from_user.id),
-            parse_mode=ParseMode.MARKDOWN_V2
+            parse_mode=ParseMode.HTML
         )
         return
-    # text = wooordhunt_data
-    await message.reply('В какую колоду сохранить?', reply_markup=await generate_deck_keyboard())
+
+    global word
+    word = parser.get_word(text)
+    if not word or not any((word.description, word.transcription, word.rank, word.examples)):
+        await message.reply('WooordHunt не знает такого слова')
+    else:
+        examples = '\n———\n'.join([f'{e[0]}\n{e[1]}' for e in word.examples][:5])
+        text = (f'<strong>{word.name}</strong>\n\n'
+                f'<strong>Перевод:</strong> {word.description}\n'
+                f'<strong>Транскрипция:</strong> {word.transcription}\n'
+                f'<strong>Популярность:</strong> {word.rank}\n\n'
+                f'<strong>Примеры:</strong>\n'
+                f'{examples}')
+        await message.reply(text,
+                            parse_mode=ParseMode.HTML,
+                            reply_markup=await generate_deck_keyboard())
 
 
 async def handle_deck_selection(callback_query: CallbackQuery) -> None:
     """
-    Handle inline keyboard button presses.
+    Handles deck selection from the inline keyboard.
 
-    This handler will be called when the user presses a button on the inline keyboard.
-    It checks the callback data to determine which deck was selected and sends a response.
+    This function processes the callback query to determine the selected deck based
+    on the callback data. It then adds the word to the selected deck, synchronizes
+    with Anki, and sends a confirmation message.
 
-    :param callback_query: The callback query object containing the callback data.
+    :param callback_query: The callback query object containing the callback data from the inline keyboard.
     """
-
     await callback_query.message.edit_text(callback_query.message.text)
 
     deck = callback_query.data
     if deck == "Don`t_Save":
         await callback_query.message.reply('Сохранение отменено')
     else:
-        await anki_api.add_note(deck, front='', back='')
+        global word
+        await anki_api.add_note(deck,
+                                front=f'{word.name}<br>{word.transcription}',
+                                back=word.description)
         await anki_api.sync()
         await callback_query.message.reply(f'Слово сохранено в колоду: {deck}')
-
     await callback_query.answer()
 
 
